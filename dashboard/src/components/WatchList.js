64e39a27dd2cc3e7f0654ce1b2d88f2ea283762f
sambit578 <sambitkumarbal24@gmail.com>
2026-01-12 19:24:26 +0530

    Ploting Chart

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -34,44 +34,18 @@ const WatchList = () => {
           "rgba(255, 159, 64, 0.5)",
         ],
         borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
+          "rgba(239, 68, 68, 1)",    
+          "rgba(59, 130, 246, 1)",   
+          "rgba(234, 179, 8, 1)",   
+          "rgba(16, 185, 129, 1)",   
+          "rgba(139, 92, 246, 1)",   
+          "rgba(249, 115, 22, 1)",   
+    ],
+       borderWidth: 1,
       },
     ],
   };
 
-  // export const data = {
-  //   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  // datasets: [
-  //   {
-  //     label: "# of Votes",
-  //     data: [12, 19, 3, 5, 2, 3],
-  //     backgroundColor: [
-  //       "rgba(255, 99, 132, 0.2)",
-  //       "rgba(54, 162, 235, 0.2)",
-  //       "rgba(255, 206, 86, 0.2)",
-  //       "rgba(75, 192, 192, 0.2)",
-  //       "rgba(153, 102, 255, 0.2)",
-  //       "rgba(255, 159, 64, 0.2)",
-  //     ],
-  //     borderColor: [
-  //       "rgba(255, 99, 132, 1)",
-  //       "rgba(54, 162, 235, 1)",
-  //       "rgba(255, 206, 86, 1)",
-  //       "rgba(75, 192, 192, 1)",
-  //       "rgba(153, 102, 255, 1)",
-  //       "rgba(255, 159, 64, 1)",
-  //     ],
-  //     borderWidth: 1,
-  //   },
-  // ],
-  // };
 
   return (
     <div className="watchlist-container">
